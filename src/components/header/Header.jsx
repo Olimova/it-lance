8964c69b35b91d9efd16746d6aec3f8c9b614c49
Olimova <ozoda4662@gmail.com>
2025-08-27 15:47:38 +0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Heder } from '../../static'
+import img from "../../assets/IMAGE (14).svg"
+
+const Header = () => {
+  return (
+    <>
+        <header className='heaader'>
+            <div className="header-wrapper">
+                  <div>
+                      <img src={img} alt="" />
+                  </div>
+                  {
+                      Heder?.map((item, inx) => (
+                          <ul key={inx}>
+                              <li className='list'>{item}</li>
+                          </ul>
+                      ))
+                  }
+                  <div className='button'>
+                      <button className='btn'>Отправить заявку</button>
+                      <button class="lang-btn">РУС</button>
+                  </div>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <section className='hero'>
+            <div className='hero-wrapper'>
+                <h1>
+                      Проектирование и установка систем видеонаблюдения
+                </h1>
+                  <p>Защитите свой бизнес от неожиданных сбоев!</p>
+                  <button className='hero-btn'>Узнать больше</button>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Hero
